959aec100a9a23f40a988fefa43712d8cc64a557
wlky <wlky080106@qq.com>
2022-07-06 00:48:52 +0800

    no message

---
+++ hpp_config.js
@@ -0,0 +1,25 @@
+// 配置
+window.Config = {
+
+  // 站点名
+  SiteName: '',
+
+  // 站点链接
+  SiteUrl: '/',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
+  ApiKeys: [
+    'ur1710890-96dcd7c14eaf8c85af693a25',
+  ],
+
+  // 是否显示监测站点的链接
+  ShowLink: true,
+
+  // 日志天数
+  // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
+  // 不过时间不要设置太长，容易卡，接口请求也容易失败
+  CountDays: 60,
+
+  // 导航栏菜单
+};
